94d994aeb48bffd43387aa8afa867caca518fe11
R.Ito <iriku.ir@gmail.com>
2024-06-27 15:45:58 +0900

    wip

--- app/src/main/java/com/example/scalesseparatefileble/ui/BLEData.kt
+++ app/src/main/java/com/example/scalesseparatefileble/ui/BLEData.kt
@@ -1,21 +1,38 @@
 package com.example.scalesseparatefileble.ui
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.runtime.*
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.model.ViewModel
 import com.example.scalesseparatefileble.bluetooth.BluetoothManager
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ThirdScreen(
     viewModel: ViewModel = hiltViewModel(),
@@ -23,39 +40,60 @@ fun ThirdScreen(
     onTapNextButton: () -> Unit = {},
     onTapBackButton: () -> Unit = {}
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                backgroundColor = Color.LightGray,
-                title = { Text(
-                    "Label: ${viewModel.label.value}",
-                    fontSize = 28.sp
-                ) },
-                navigationIcon = {
-                    IconButton(onClick = { onTapBackButton() }) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = "戻る")
-                    }
+    LaunchedEffect(Unit, bluetoothManager) {
+        bluetoothManager.initializeBluetooth()
+    }
+
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.HalfExpanded)
+
+    val coroutineScope = rememberCoroutineScope()
+    BackHandler(sheetState.isVisible) {
+        coroutineScope.launch { sheetState.show() }
+    }
+
+    ModalBottomSheetLayout(
+        sheetContent = {
+            NotificationData(viewModel = viewModel, bluetoothManager = bluetoothManager)
+            Button(
+                onClick = { viewModel.undoRemoval() },
+                modifier = Modifier
+                    .padding(vertical = 8.dp)) {
+                Text("値を戻す",fontSize = 20.sp)
+            }
+            Button(
+                onClick = {
+                    viewModel.saveDataCsv()
+//            Toast.makeText(context, "これはトーストです", Toast.LENGTH_SHORT).show()
                 },
-                actions = {
-                    IconButton(onClick = { onTapNextButton() }) {
-                        Icon(Icons.Default.ArrowForward, contentDescription = "次へ")
-                    }
-                }
-            )
-        }
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+            ) {
+                Text("保存",fontSize = 20.sp)
+            }
+            Button(
+                onClick = onTapNextButton,
+                modifier = Modifier
+                    .padding(vertical = 16.dp)
+            ) {
+                Text(text = "Next",fontSize = 20.sp)
+            }
+        },
+        sheetState = sheetState,
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .imePadding()
+            .navigationBarsPadding()
     ) {
         BLEData(
             viewModel = viewModel,
-            bluetoothManager = bluetoothManager,
-            onClickButton = onTapNextButton,
-            paddingValues = it
+            bluetoothManager = bluetoothManager
         )
-
     }
 }
 
 @Composable
-fun BLEData(viewModel: ViewModel, bluetoothManager: BluetoothManager, onClickButton: () -> Unit, paddingValues: PaddingValues){
+fun BLEData(viewModel: ViewModel, bluetoothManager: BluetoothManager){
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
@@ -63,14 +101,12 @@ fun BLEData(viewModel: ViewModel, bluetoothManager: BluetoothManager, onClickBut
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
-                .padding(paddingValues),
+                .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
-            Text(
-                text = bluetoothManager.bluetoothUtilities.bleStateMessage.value,
-                fontSize = 28.sp)
+            Text(text = viewModel.label.value, fontSize = 28.sp)
+
             Box(
                 modifier = Modifier
                     .size(480.dp)
@@ -80,32 +116,8 @@ fun BLEData(viewModel: ViewModel, bluetoothManager: BluetoothManager, onClickBut
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    ContentScreen(viewModel)
+                    ContentScreen(viewModel = viewModel)
                 }
-            }
-            NotificationData(viewModel = viewModel, bluetoothManager = bluetoothManager)
-            OutlinedButton(
-                onClick = { viewModel.undoRemoval() },
-                modifier = Modifier
-                    .padding(vertical = 8.dp)) {
-                Text("値を戻す",fontSize = 20.sp)
-            }
-            Button(
-                onClick = {
-                viewModel.saveDataCsv()
-//            Toast.makeText(context, "これはトーストです", Toast.LENGTH_SHORT).show()
-                },
-                modifier = Modifier
-                    .padding(vertical = 8.dp)
-            ) {
-                Text("保存",fontSize = 20.sp)
-            }
-            Button(
-                onClick = onClickButton,
-                modifier = Modifier
-                    .padding(vertical = 16.dp)
-            ) {
-                Text(text = "Next",fontSize = 20.sp)
             }
         }
     }
--- app/src/main/java/com/example/scalesseparatefileble/ui/BLEDataList.kt
+++ app/src/main/java/com/example/scalesseparatefileble/ui/BLEDataList.kt
@@ -3,14 +3,31 @@ package com.example.scalesseparatefileble.ui
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.animateScrollBy
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -18,8 +35,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.scalesseparatefileble.util.CustomSwipeToDismiss
 import com.example.model.ViewModel
+import com.example.scalesseparatefileble.util.CustomSwipeToDismiss
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -30,7 +47,8 @@ fun ContentScreen(viewModel: ViewModel) {
     Column(modifier = Modifier.fillMaxSize()) {
         LazyColumn(
             modifier = Modifier.weight(1f),
-            contentPadding = PaddingValues(16.dp),
+            contentPadding = PaddingValues(vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
             state = listState
         ) {
             itemsIndexed(items = viewModel.items, key = { _, item -> item.id }) { index, item ->
@@ -42,6 +60,7 @@ fun ContentScreen(viewModel: ViewModel) {
                         true
                     }
                 )
+
                 CustomSwipeToDismiss(
                     state = dismissState,
                     directions = setOf(DismissDirection.EndToStart),
@@ -50,9 +69,9 @@ fun ContentScreen(viewModel: ViewModel) {
                         Box(
                             modifier = Modifier
                                 .fillMaxSize()
-                                .clip(RoundedCornerShape(8.dp)) // 最初にclipを適用
+                                .clip(RoundedCornerShape(14.dp)) // 最初にclipを適用
                                 .background(Color.Red)
-                                .padding(4.dp), // clipの後にpaddingを適用
+                                .padding(5.dp), // clipの後にpaddingを適用
                             contentAlignment = Alignment.CenterEnd
                         ) {
                             Icon(
@@ -64,21 +83,19 @@ fun ContentScreen(viewModel: ViewModel) {
                         }
                     },
                     dismissContent = {
-                        Card(
+                        Row(
                             modifier = Modifier
-                                .fillMaxWidth(),
+                                .fillMaxWidth()
+                                .height(56.dp)
+                                .background(Color(0xFFF0F0F0), RoundedCornerShape(12.dp))
+                                .padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp),
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(text = "${index + 1}. ", style = MaterialTheme.typography.h6, modifier = Modifier.weight(1f))
-                                Spacer(modifier = Modifier.width(16.dp))
-                                Text(text = "${item.value}", style = MaterialTheme.typography.body1, modifier = Modifier.weight(2f))
-                            }
+                            Text(text = "${index + 1}. ", style = MaterialTheme.typography.h6, modifier = Modifier.weight(1f))
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Text(text = item.value, style = MaterialTheme.typography.body1, modifier = Modifier.weight(2f))
                         }
+
                     }
                 )
             }
